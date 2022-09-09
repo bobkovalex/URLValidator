2e46048d63d85c451dd8e90bd401c08f46d7d861
Svarog07 <asup07@rambler.ru>
2022-09-09 13:46:07 +0300

    W.I.P started investigation of the queue

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const port = process.env.PORT || 8000;
 var bodyParser = require('body-parser');
-var tools = require('./tools');
+
+const Arena = require('bull-arena');
+// Mandatory import of queue library.
+const Bee = require('bee-queue');
+const Queue = require('bee-queue');
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var app = express();
 app.use(express.static('public'));
+
+
+const arenaConfig = Arena({
+    // All queue libraries used must be explicitly imported and included.
+    Bee,
+
+    // Provide a `Bull` option when using bull, similar to the `Bee` option above.
+    queues: [
+        {
+            // Required for each queue definition.
+            name: 'queue',
+
+            // User-readable display name for the host. Required.
+            hostId: 'Queue Server 1',
+
+            // Queue type (Bull or Bee - default Bull).
+            type: 'bee',
+
+            // Queue key prefix. Defaults to "bq" for Bee and "bull" for Bull.
+            prefix: 'foo',
+        },
+    ],
+ 
+},
+    {
+        // Make the arena dashboard become available at {my-site.com}/arena.
+        basePath: '/arena',
+
+        // Let express handle the listening.
+        disableListen: true,
+    });
+
+// Make arena's resources (js/css deps) available at the base app route
+app.use('/', arenaConfig);
 
 // Index Page
 app.get('/', function (req, res) {
